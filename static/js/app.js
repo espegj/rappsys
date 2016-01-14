1ecfbba397d6c15b2ae20fc842c26b00971786ce
Thomas From <thomas.af92@gmail.com>
2016-01-14 12:01:09 +0100

    Søkefelt + småfiks design

---
+++ static/js/app.js
@@ -0,0 +1,12 @@
+var options = {
+  valueNames: [ 'name', 'description' ]
+};
+
+var projectList = new List('projects', options);
+
+var options1 = {
+  valueNames: [ 'name', 'description' ]
+};
+
+var activityList = new List('activities', options1);
+
